6dc4b66dd84355b515a023ce37778a54cacb89c7
sk-shahsi <sk.sashikumar055@gmail.com>
2025-10-22 12:04:59 +0530

    uplode some code

---
+++ simple_interest.py
@@ -0,0 +1,11 @@
+"""
+simple interest =(p* R * T)/100
+p= Principal amount
+R= rate of interest
+T= time duration
+"""
+principle = float(input("Enter principle amount: "))
+rate = float(input("Enter rate of interest: "))
+time = float(input("Enter time duration: "))
+si=(principle*rate*time)/100
+print(si)
---
+++ string.py
@@ -0,0 +1,24 @@
+ # s="Hello World"
+# # length of string
+# a= len(s)
+# print(a)
+#
+# # indexing of String
+# print(s[0])
+# print(s[-1])
+# print(s[2])
+
+
+"""
+slicing
+Syntex: indexing: string[index]
+synteax of slicing: String[str:end:stop]
+start: starting index at which the slicing operation starts.
+end: ending index at which the slicing operation ends.
+stop: integer that specified the stop fro the slicing operation.
+
+"""
+s="Hello World"
+print(s[0:11:2]) #HloWrd
+
+
